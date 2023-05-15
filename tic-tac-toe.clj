10d1213f121545f470004159a6a69d1d6ff39ef0
Armando Serrato <rsb987@hotmail.com>
2023-05-15 15:33:45 -0600

    tictactoe move func

--- tic-tac-toe.clj
+++ tic-tac-toe.clj
@@ -1,4 +1,21 @@
-(def init_board ["O" " " "X" " " "O" " " " " "X" "O"])
+(def empty_cell " ")
+
+(def init_board [empty_cell empty_cell empty_cell empty_cell empty_cell empty_cell empty_cell empty_cell empty_cell])
+
+(defn get_board_pos [col row]
+    (+ (* row 3) col)
+)
+(defn get_symbol [board col row]
+    (get board (get_board_pos col row)) ;;=index = (row * 3) + col
+)
+(defn move [board col row new_symbol]
+    (let [symbol (get_symbol board col row)]
+        (if (clojure.string/blank? symbol)
+            (assoc board (get_board_pos col row) new_symbol)
+            board
+        )
+    )
+)
 
 (defn str_line_board [board index](
     str (get board index) " | " (get board (+ index 1)) " | " (get board (+ index 2)) 
@@ -6,15 +23,16 @@
 
 (defn draw [board] (
         (println (str_line_board board 0))
-        (println "------")
+        (println "--------")
         (println (str_line_board board 3))
-        (println "------")
+        (println "--------")
         (println (str_line_board board 6))
     )
 )
 
+
 (defn main [](
-    draw init_board
+    (draw (move init_board 2 2 "X"))
 ))
 
 (main)
