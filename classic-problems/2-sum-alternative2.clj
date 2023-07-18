42c13a290f868095b44e6119575ae252ab8dc9b7
Armando Serrato <rsb987@hotmail.com>
2023-07-18 13:51:32 -0600

    2-sum improvement

--- classic-problems/2-sum-alternative2.clj
+++ classic-problems/2-sum-alternative2.clj
@@ -1,17 +1,8 @@
-
-(defn success [i j target array]
-  (let [n1 (get array i) n2 (get array j)]
-    (if (= target (+ n1 n2))
-      [n1 n2]
-      nil)))
-
 (defn two-sum [target array]
   (let [n (count array)]
     (for [i (range n) j (range i n)]
-      (let [goal (success i j target array)]
-        (if (some? goal)
-          goal
-          )))))
+      (if (= target (+ (get array i) (get array j)))
+        [(get array i) (get array j)]))))
 
 (defn solve [target array]
   (filter some? (two-sum target array)))
