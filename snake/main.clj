ce109022bb4927e50f2d5aa4938d072ccb407cf4
Armando Serrato <rsb987@hotmail.com>
2023-12-18 14:00:28 -0600

    snake: move dot

--- snake/main.clj
+++ snake/main.clj
@@ -5,11 +5,38 @@
         (java.awt.event KeyAdapter)
         (java.awt.event KeyEvent))
 
-(defn draw [g]
- (do
-   (.drawOval g 300 300 100 100)
-   (.fillOval g 300 300 100 100)))
+(def speedx 5)
+(def speedy 5)
 
+(defn init-world []
+ {:x 100 :y 100} )
+
+(defn translate [world deltax deltay]
+  (let [x (get world :x) y (get world :y)]
+    {:x (+ (* speedx deltax) x) :y (+ (* speedy deltay) y)}))
+
+(defn go-right [world]
+  (translate world 1 0))
+
+(defn go-left [world]
+  (translate world -1 0))
+
+(defn go-up [world]
+  (translate world 0 -1))
+
+(defn go-down [world]
+  (translate world 0 1))
+
+(defn draw [world g]
+ (do
+   (let [x (get world :x) 
+         y (get world :y)
+         bounds (.getClipBounds g)
+         width (.getWidth bounds)
+         height (.getHeight bounds)]
+   (.clearRect g 0 0 width height)
+   (.drawOval g x y 50 50)
+   (.fillOval g x y 50 50))))
 
 (defn getKeyPressed [e]
   (let [code (.getKeyCode e)]
@@ -20,12 +47,19 @@
       (= java.awt.event.KeyEvent/VK_LEFT code) "left"
       :else "none")))
 
-(defn onKeyStroke [e]
-  (println (getKeyPressed e))) 
+(defn onKeyStroke [world e]
+  (let [move (getKeyPressed e)]
+   (cond
+     (= move "up") (go-up world)
+     (= move "down") (go-down world)
+     (= move "right") (go-right world)
+     (= move "left") (go-left world)
+     :else world)))
 
 (defn main []
-  (let [frame (JFrame.)
-        panel (proxy [JPanel] [] (paintComponent [g] (draw g)))]
+  (let [world (atom (init-world))
+        frame (JFrame.)
+        panel (proxy [JPanel] [] (paintComponent [g] (draw @world g)))]
     (doto frame
       (.setDefaultCloseOperation
         javax.swing.WindowConstants/EXIT_ON_CLOSE)
@@ -36,9 +70,10 @@
       (.addKeyListener
         (proxy [KeyAdapter] []
           (keyPressed [e]
-            (onKeyStroke e))))
-          (.requestFocus))
+            (let [new-world "hol"]
+              (swap! world  (fn [w] (onKeyStroke w e)))
+              (.repaint panel))))))
     (.repaint panel)
-    ))
+    (.requestFocus panel)))
 
 (main)
